f5225fac3c51bc20133eb25940b9b110276024bc
Vlad <vladukhatyuk@gmail.com>
2020-02-08 21:15:23 +0000

    tidy up

--- lambda/hitcount.py
+++ lambda/hitcount.py
@@ -9,3 +9,16 @@ _lambda = boto3.client('lambda')
 
 
 def handler(event, context):
+    table.update_item(
+        Key = {'path': event['path']},
+        UpdateExpression= 'ADD hits :incr',
+        ExpressionUAttributeValues = {':incr:1'}
+    )
+
+    resp = _lambda.invoke(
+        FunctionName=os.environ['DOWNSTREAM_FUNCTION_NAME'],
+        Payload=json.dumps(event)
+    )
+
+    body = resp['Payload'].read()
+    return json.loads(body)
--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,21 +1,20 @@
-import cdk = require('@aws-cdk/core');
-import lambda = require('@aws-cdk/aws-lambda');
-import apigw = require('@aws-cdk/aws-apigateway')
+import cdk = require("@aws-cdk/core");
+import lambda = require("@aws-cdk/aws-lambda");
+import apigw = require("@aws-cdk/aws-apigateway");
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // defines an AWS Lambda resource
-    const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.PYTHON_3_7,      // execution environment
-      code: lambda.Code.asset('lambda'),  // code loaded from the "lambda" directory
-      handler: 'hello.handler'                // file is "hello", function is "handler"
+    const hello = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.PYTHON_3_7, // execution environment
+      code: lambda.Code.asset("lambda"), // code loaded from the "lambda" directory
+      handler: "hello.handler" // file is "hello", function is "handler"
     });
 
-    new apigw.LambdaRestApi(this, "Endpoint",{
-      handler:hello
-    })
-
+    new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: hello
+    });
   }
 }
