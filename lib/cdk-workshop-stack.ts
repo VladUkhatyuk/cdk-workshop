8976c08747d09527c3eba8a7f378dd4f193de835
Vlad <vladukhatyuk@gmail.com>
2020-02-08 21:39:29 +0000

    added handler to stack

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,6 +1,7 @@
 import cdk = require("@aws-cdk/core");
 import lambda = require("@aws-cdk/aws-lambda");
 import apigw = require("@aws-cdk/aws-apigateway");
+import { HitCounter } from "./hitcounter";
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -11,6 +12,10 @@ export class CdkWorkshopStack extends cdk.Stack {
       runtime: lambda.Runtime.PYTHON_3_7, // execution environment
       code: lambda.Code.asset("lambda"), // code loaded from the "lambda" directory
       handler: "hello.handler" // file is "hello", function is "handler"
+    });
+
+    const helloWorldHitCounter = new HitCounter(this, "HelloHitCounter", {
+      downstream: hello
     });
 
     new apigw.LambdaRestApi(this, "Endpoint", {
--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -1,12 +1,29 @@
-import cdk = require('@aws-cdk/core')
-import lambda = require('@aws-cdk/aws-lambda')
+import cdk = require("@aws-cdk/core");
+import lambda = require("@aws-cdk/aws-lambda");
+import dynamodb = require("@aws-cdk/aws-dynamodb");
 
 export interface HitCounterProps {
-    downstream: lambda.IFunction
+  downstream: lambda.IFunction;
 }
 
-export class HitCounter extends cdk.Construct{
-    constructor(scope:cdk.Construct, id: string, props: HitCounterProps){
-        super(scope,id);
-    }
+export class HitCounter extends cdk.Construct {
+  public readonly handler: lambda.Function;
+
+  constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
+    super(scope, id);
+
+    const table = new dynamodb.Table(this, "Hits", {
+      partitionKey: { name: "path", type: dynamodb.AttributeType.STRING }
+    });
+
+    this.handler = new lambda.Function(this, "HitCountHandler", {
+      runtime: lambda.Runtime.PYTHON_3_7,
+      handler: "hitcounter_handler",
+      code: lambda.Code.asset("lambda"),
+      environment: {
+        DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+        HITS_TABLE_NAME: table.tableName
+      }
+    });
+  }
 }
