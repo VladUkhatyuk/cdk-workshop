6cf8fd65e3c684dede5fb1a02b0b8bbdc282988f
Vlad <vladukhatyuk@gmail.com>
2020-02-10 20:04:01 +0000

    added dynamo db table with sorting

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -2,6 +2,7 @@ import cdk = require('@aws-cdk/core');
 import lambda = require('@aws-cdk/aws-lambda');
 import apigw = require('@aws-cdk/aws-apigateway');
 import { HitCounter } from './hitcounter';
+import {TableViewer} from 'cdk-dynamo-table-viewer';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -21,5 +22,11 @@ export class CdkWorkshopStack extends cdk.Stack {
     new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: helloWithCounter.handler
     });
+
+    new TableViewer(this,'viewHitCounter',{
+      title: 'Hello Hits',
+      table:helloWithCounter.table,
+      sortBy:'-hits'
+    })
   }
 }
--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -1,6 +1,6 @@
-import cdk = require('@aws-cdk/core');
-import lambda = require('@aws-cdk/aws-lambda');
-import dynamodb = require('@aws-cdk/aws-dynamodb');
+import cdk = require("@aws-cdk/core");
+import lambda = require("@aws-cdk/aws-lambda");
+import dynamodb = require("@aws-cdk/aws-dynamodb");
 
 export interface HitCounterProps {
   /** the function for which we want to count url hits **/
@@ -8,30 +8,32 @@ export interface HitCounterProps {
 }
 
 export class HitCounter extends cdk.Construct {
-
   /** allows accessing the counter function */
   public readonly handler: lambda.Function;
 
-  constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
-      super(scope, id);
+  public readonly table: dynamodb.Table;
 
-    const table = new dynamodb.Table(this, 'Hits', {
-        partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
+  constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
+    super(scope, id);
+
+    const table = new dynamodb.Table(this, "Hits", {
+      partitionKey: { name: "path", type: dynamodb.AttributeType.STRING }
     });
 
-    this.handler = new lambda.Function(this, 'HitCounterHandler', {
-        runtime: lambda.Runtime.PYTHON_3_7,
-        handler: 'hitcount.handler',
-        code: lambda.Code.asset('lambda'),
-        environment: {
-            DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
-            HITS_TABLE_NAME: table.tableName
-        }
+    this.table = table;
+
+    this.handler = new lambda.Function(this, "HitCounterHandler", {
+      runtime: lambda.Runtime.PYTHON_3_7,
+      handler: "hitcount.handler",
+      code: lambda.Code.asset("lambda"),
+      environment: {
+        DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+        HITS_TABLE_NAME: table.tableName
+      }
     });
     // grant the lambda role read/write permissions to our table
     table.grantReadWriteData(this.handler);
-    
-    props.downstream.grantInvoke(this.handler);
 
+    props.downstream.grantInvoke(this.handler);
   }
 }
